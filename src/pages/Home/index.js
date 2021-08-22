ffd5ba7ceb44f6c3ab2e46db220f532aa35e2ada
David Garcia <davidjosue.garcia@outlook.com>
2021-08-21 22:33:42 -0600

    Creando Home Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Container,
+  Card,
+  Grid,
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core';
 
 const Home = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleCleanTextClick = () => {};
+
+  const handleSearchTextClick = () => {};
+
   return (
     <Container>
-      <Typography variant="h1">Home</Typography>
+      <Card>
+        <Grid container>
+          <Grid>
+            <Typography>Bienvenido</Typography>
+          </Grid>
+          <Grid>
+            <label>Icono</label>
+          </Grid>
+        </Grid>
+
+        <TextField
+          value={searchText}
+          placeholder="Buscar"
+          onChange={handleSearchTextChange}
+        />
+
+        <Grid>
+          <Button
+            variant="contained"
+            onClick={handleCleanTextClick}
+          >
+            Limpiar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSearchTextClick}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Card>
     </Container>
   );
 };
