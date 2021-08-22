2b0370f7819ce131b8a660808992be5c2b04af60
David Garcia <davidjosue.garcia@outlook.com>
2021-08-21 22:15:57 -0600

    Instalando Material.UI

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container>
+      <Typography variant="h1">Home</Typography>
+    </Container>
   );
 };
 
